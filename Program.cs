521e24f846a348894fe80ab20bf2d9e15842e4e4
Mayara A <mayara.arj@gmail.com>
2022-06-09 16:24:25 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿double cotacaoDoDolarHoje = 4.97;
+const double VALOR_MINIMO_DE_COMPRA_EM_DOLAR = 100;
+const double VALOR_MAXIMO_DE_COMPRA_EM_DOLAR = 2500;
+
+
+
+string converterRealParaDolar(double valorDaCompraEmReais)
+{
+    double resultadoConversao;
+    resultadoConversao = valorDaCompraEmReais / cotacaoDoDolarHoje;
+
+    if (resultadoConversao < VALOR_MINIMO_DE_COMPRA_EM_DOLAR)
+    {
+        return "A compra mínima permitida é de $ 100,00.";
+    }
+    else if (resultadoConversao > VALOR_MAXIMO_DE_COMPRA_EM_DOLAR)
+    {
+        return "A compra máxima permitida é de $ 2500,00.";
+    }
+    else
+    {
+        return $"Você pode comprar  ${resultadoConversao}";
+    }
+}
+
+
+
+Console.WriteLine(converterRealParaDolar(700));
